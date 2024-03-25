a2d44eba5fceff5e329eb86c06d0dae835d2f19e
SebastianObert <163373214+SebastianObert@users.noreply.github.com>
2024-03-25 15:33:25 +0700

    Update main.js

--- pages/main.js
+++ pages/main.js
@@ -5,4 +5,29 @@ document.addEventListener('DOMContentLoaded', function() {
     if (savedName) {
         userNameElement.textContent = `Hello, ${savedName}`;
     }
+
+    const transactionName = localStorage.getItem('transactionName');
+    const transactionNominal = localStorage.getItem('transactionNominal');
+    const transactionCategory = localStorage.getItem('transactionCategory');
+
+    if (transactionName && transactionNominal && transactionCategory) {
+        const transactionInfo = document.querySelector('.transaction .info');
+        transactionInfo.innerHTML = `
+            <p>Nama Transaksi: ${transactionName}</p>
+            <p>Nominal Transaksi: ${transactionNominal}</p>
+            <p>Kategori Transaksi: ${transactionCategory}</p>
+        `;
+
+        localStorage.removeItem('transactionName');
+        localStorage.removeItem('transactionNominal');
+        localStorage.removeItem('transactionCategory');
+    } else {
+
+        const transactionInfo = document.querySelector('.transaction .info');
+        transactionInfo.textContent = 'Belum ada transaksi';
+    }
+});
+
+window.addEventListener('beforeunload', function() {
+    localStorage.clear();
 });
